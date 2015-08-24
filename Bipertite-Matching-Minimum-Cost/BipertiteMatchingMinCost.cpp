6c1589644cb3b903a4d0722736358a2f1b2c54de
Abu Asif Khan Chowdhury <abuasifkhan@gmail.com>
2015-08-24 15:17:32 +0600

    Create BipertiteMatchingMinCost.cpp

---
+++ Bipertite-Matching-Minimum-Cost/BipertiteMatchingMinCost.cpp
@@ -0,0 +1,196 @@
+//! Bismillahi-Rahamanirahim.
+
+///////////////////////////////////////////////////////////////////////////
+// Min cost bipartite matching via shortest augmenting paths
+//
+// This is an O(n^3) implementation of a shortest augmenting path
+// algorithm for finding min cost perfect matchings in dense
+// graphs.  In practice, it solves 1000x1000 problems in around 1
+// second.
+//
+//   cost[i][j] = cost for pairing left node i with right node j
+//   Lmate[i] = index of right node that left node i pairs with
+//   Rmate[j] = index of left node that right node j pairs with
+//
+// The values in cost[i][j] may be positive or negative.  To perform
+// maximization, simply negate the cost[][] matrix.
+///////////////////////////////////////////////////////////////////////////
+
+/** ========================================**
+ ** @Author: A Asif Khan Chowdhury
+/** ========================================**/
+
+
+/**................ Headers ................**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**................ Macros ................**/
+#define Set(N, j) (N|(1<<j))
+#define reset(N, j) (N&~(1<<j))
+#define Check(N, j) (bool)(N&(1<<j))
+#define toggle(N, j) (N^(1<<j))
+#define turnOff(N, j) (N & ~(1<<j))
+#define CLEAR(A, x) ( memset(A,x,sizeof(A)) )
+#define pi 3.141592653589793
+#define pii pair <int, int>
+#define pb push_back
+#define pf printf
+#define S(x) scanf("%d", &x)
+#define SL(x) scanf("%ld", &x)
+#define out(C) printf("Case %d: ", C);
+#define FOR(i, x, y) for(int i=x; i<=y; i++)
+#define open freopen("D:/a.txt", "r", stdin);
+#define write freopen("D:/b.txt","w", stdout);
+#define MAX (1+(1<<5))
+#define inf 0x7fffffff
+#define ll long long
+#define mod 1000000007
+#define gc getchar
+#define ls(n) (n<<1)
+#define rs(n) ls(n)|1
+#define MID(a,b) ((a+b)>>1)
+#define mx 100010
+
+//Fast Reader
+template<class T>inline bool read(T &x) {
+    int c=getchar();
+    int sgn=1;
+    while(~c&&c<'0'||c>'9') {
+        if(c=='-')sgn=-1;
+        c=getchar();
+    }
+    for(x=0; ~c&&'0'<=c&&c<='9'; c=getchar())x=x*10+c-'0';
+    x*=sgn;
+    return ~c;
+}
+int X[]= {-1, -1, -1, 0, 1, 1, 1, 0};   //x 8 direction
+int Y[]= {-1, 0, +1, 1, 1, 0, -1, -1};  //y 8 direction
+// int X[]= {-1, 0, 1, 0};   //x 4 direction
+// int Y[]= { 0, 1, 0, -1};  //y 4 direction
+
+typedef vector<double> VD;
+typedef vector<VD> VVD;
+typedef vector<int> VI;
+
+double MinCostMatching(const VVD &cost, VI &Lmate, VI &Rmate) {
+  int n = int(cost.size());
+
+  // construct dual feasible solution
+  VD u(n);
+  VD v(n);
+  for (int i = 0; i < n; i++) {
+    u[i] = cost[i][0];
+    for (int j = 1; j < n; j++) u[i] = min(u[i], cost[i][j]);
+  }
+  for (int j = 0; j < n; j++) {
+    v[j] = cost[0][j] - u[0];
+    for (int i = 1; i < n; i++) v[j] = min(v[j], cost[i][j] - u[i]);
+  }
+
+  // construct primal solution satisfying complementary slackness
+  Lmate = VI(n, -1);
+  Rmate = VI(n, -1);
+  int mated = 0;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (Rmate[j] != -1) continue;
+      if (fabs(cost[i][j] - u[i] - v[j]) < 1e-10) {
+	Lmate[i] = j;
+	Rmate[j] = i;
+	mated++;
+	break;
+      }
+    }
+  }
+
+  VD dist(n);
+  VI dad(n);
+  VI seen(n);
+
+  // repeat until primal solution is feasible
+  while (mated < n) {
+
+    // find an unmatched left node
+    int s = 0;
+    while (Lmate[s] != -1) s++;
+
+    // initialize Dijkstra
+    fill(dad.begin(), dad.end(), -1);
+    fill(seen.begin(), seen.end(), 0);
+    for (int k = 0; k < n; k++)
+      dist[k] = cost[s][k] - u[s] - v[k];
+
+    int j = 0;
+    while (true) {
+
+      // find closest
+      j = -1;
+      for (int k = 0; k < n; k++) {
+	if (seen[k]) continue;
+	if (j == -1 || dist[k] < dist[j]) j = k;
+      }
+      seen[j] = 1;
+
+      // termination condition
+      if (Rmate[j] == -1) break;
+
+      // relax neighbors
+      const int i = Rmate[j];
+      for (int k = 0; k < n; k++) {
+	if (seen[k]) continue;
+	const double new_dist = dist[j] + cost[i][k] - u[i] - v[k];
+	if (dist[k] > new_dist) {
+	  dist[k] = new_dist;
+	  dad[k] = j;
+	}
+      }
+    }
+
+    // update dual variables
+    for (int k = 0; k < n; k++) {
+      if (k == j || !seen[k]) continue;
+      const int i = Rmate[k];
+      v[k] += dist[k] - dist[j];
+      u[i] -= dist[k] - dist[j];
+    }
+    u[s] += dist[j];
+
+    // augment along path
+    while (dad[j] >= 0) {
+      const int d = dad[j];
+      Rmate[j] = Rmate[d];
+      Lmate[Rmate[j]] = j;
+      j = d;
+    }
+    Rmate[j] = s;
+    Lmate[s] = j;
+
+    mated++;
+  }
+
+  double value = 0;
+  for (int i = 0; i < n; i++)
+    value += cost[i][Lmate[i]];
+
+  return value;
+}
+
+
+int main() {
+	#ifdef LOCAL
+    open
+    #endif // LOCAL
+    int test;
+    read(test);
+    FOR(C, 1, test) {
+        out(C);
+    }
+    return 0;
+}
+
+
+
+
+
