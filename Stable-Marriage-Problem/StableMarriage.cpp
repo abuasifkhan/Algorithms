54a279e59bb48cbbaaa9de6a133b4aef8043e6a7
Abu Asif Khan Chowdhury <abuasifkhan@gmail.com>
2015-09-27 19:44:36 +0600

    Update StableMarriage.cpp

--- Stable-Marriage-Problem/StableMarriage.cpp
+++ Stable-Marriage-Problem/StableMarriage.cpp
@@ -3,50 +3,6 @@
  ** @Author: A Asif Khan Chowdhury
 /** ========================================**/
 
-
-/**................ Headers ................**/
-#include <bits/stdc++.h>
-
-using namespace std;
-
-/**................ Macros ................**/
-#define Set(N, j) (N|(1<<j))
-#define reset(N, j) (N&~(1<<j))
-#define Check(N, j) (bool)(N&(1<<j))
-#define toggle(N, j) (N^(1<<j))
-#define turnOff(N, j) (N & ~(1<<j))
-#define CLEAR(A, x) ( memset(A,x,sizeof(A)) )
-#define pii pair <int, int>
-#define pb push_back
-#define open freopen("D:/a.txt", "r", stdin);
-#define write freopen("D:/b.txt","w", stdout);
-#define inf (1<<30)
-#define ll long long
-#define mod 1000000007
-#define gc getchar
-#define debug_array(a,n) for(int i=0;i<n;i++) cout<<a[i].in<<" "; cout<<endl;
-#define ls(n) (n<<1)
-#define rs(n) ls(n)|1
-#define MID(a,b) ((a+b)>>1)
-#define mx 410
-int n=4;
-//Fast Reader
-template<class T>inline bool read(T &x) {
-    int c=getchar();
-    int sgn=1;
-    while(~c&&c<'0'||c>'9') {
-        if(c=='-')sgn=-1;
-        c=getchar();
-    }
-    for(x=0; ~c&&'0'<=c&&c<='9'; c=getchar())x=x*10+c-'0';
-    x*=sgn;
-    return ~c;
-}
-int X[]= {-1, -1, -1, 0, 1, 1, 1, 0};   //x 8 direction
-int Y[]= {-1, 0, +1, 1, 1, 0, -1, -1};  //y 8 direction
-// int X[]= {-1, 0, 1, 0};   //x 4 direction
-// int Y[]= { 0, 1, 0, -1};  //y 4 direction
-
 void solve(int arr[][n]){
     int status[2*n];
     /*status[i] contains husband/wife of i, initially -1*/
@@ -97,10 +53,6 @@ int main() {
     open
     double st=clock(),en;
     #endif // LOCAL
-    // Can you solve it by brute force?
-    // LDC
-    // Are you sure you want to code so soon?? -_-
-    // Simplify your code a little more. :)
 
     // 0 to 3 are men, 4 to 7 are women. Descending order priority
     int prefer[][n]={
